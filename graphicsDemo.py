7058897d6d055a318e8e8155ff21d827070cf73b
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-20 13:38:42 -0400

    Updated from Brython Server: 9/20/2017 1:38:41 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -1,1 +1,18 @@
+#Pedro Gallino
+#9/20.17
+#graphicsDemo.py - intro to graphics using ggam
 
+from ggame import *
+
+red = Color(0xff0000,1) #(color, transperancy)
+green = Color(0x00ff00,1)
+blue = Color(0x000ff,1)
+black = Color(0x000ff,1)
+
+blackOutline = LineStyle(1,black) #(pixels, color)
+
+redRectangle = RectangleAsset(200,100,blackOutline,red) #(width, height, outline, fill)
+
+Sprite(redRectangle)
+
+App().run()
