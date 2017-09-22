5c4843baae5a2aa3a32832fed0080097aa1dcd7b
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-22 12:11:01 -0400

    Updated from Brython Server: 9/22/2017 12:11:00 PM

--- house.py
+++ house.py
@@ -2,6 +2,11 @@
 #9/20.17
 #house.py - shows a house
 
+#blueCircle = CircleAsset(50,blackOutline,blue)
+#greenEllipse = EllipseAsset(100,50,blackOutline,green)
+
+#Sprite(blueCircle,(700,50))
+#Sprite(greenEllipse,(700,200))
 
 from ggame import *
 
@@ -9,19 +14,20 @@ red = Color(0xff0000,1) #(color, transperancy)
 green = Color(0x00ff00,1)
 blue = Color(0x000ff,1)
 black = Color(0x00000,1)
+cyan = Color(0xfffff,1)
+brown = Color(0x98B4513,1)
 
-blackOutline = LineStyle(10,black) #(pixels, color)
+blackOutline = LineStyle(2,black) #(pixels, color)
 
-redRectangle = RectangleAsset(100,100,blackOutline,red)
-blueCircle = CircleAsset(50,blackOutline,blue)
-greenEllipse = EllipseAsset(100,50,blackOutline,green)
-blackLine = LineAsset(50,160,blackOutline)
-redTriangle = PolygonAsset([(0,0),(120,180),(60,300)],blackOutline,red)
+brownDoor = RectangleAsset(80,100,blackOutline,brown)
+brownDoor2 = CircleAsset(40,blackOutline,brown)
+redRectangle = RectangleAsset(400,300,blackOutline,red)
+blackRectangle = RectangleAsset(50,100,blackOutline,black)
+blackTriangle = PolygonAsset([(0,0),(200,-200),(400,0)],blackOutline,black)
 
-Sprite(redRectangle,(175,100))
-Sprite(blueCircle,(700,50))
-Sprite(greenEllipse,(700,200))
-Sprite(blackLine)
-Sprite(redTriangle)
-Sprite(text,(300,200))
+Sprite(redRectangle,(175,250))
+Sprite(blackRectangle,(475,100))
+Sprite(blackTriangle,(175,250))
+Sprite(brownDoor2,(375,430))
+Sprite(brownDoor,(335,430))
 App().run()
