8c36977732befba49ae84de0145b1095ee188e36
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-25 14:45:12 -0400

    Updated from Brython Server: 9/25/2017 2:45:11 PM

--- yield.py
+++ yield.py
@@ -5,3 +5,17 @@
 from ggame import *
 
 red = Color(0xff0000,1)
+white = Color(0xffffff,1)
+
+redOutline = LineStyle(1,red)
+whiteOutline = LineStyle(1,white)
+
+redTriangle = PolygonAsset([(0,0),(220,400),(440,0)],redOutline,red)
+whiteTriangle = PolygonAsset([(-30,-40),(110,200),(250,-40)],whiteOutline,white)
+text = TextAsset('YIELD', fill=red, style='bold 40pt Times')
+
+Sprite(redTriangle,(325,100))
+Sprite(whiteTriangle,(435,200))
+Sprite(text,(464,180))
+
+App().run()
