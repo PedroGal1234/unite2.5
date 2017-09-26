b3394f2d1e5e4354adb78dd3ff5ea8095df1f6e7
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-26 14:23:36 -0400

    Updated from Brython Server: 9/26/2017 2:23:35 PM

--- warmUp5.py
+++ warmUp5.py
@@ -1,1 +1,20 @@
+#Pedro Gallino
+#9/20/17
+#warmUp5.py - shows a dimond with name
+
+from ggame import *
+
+
+yellow = Color(0xffff00,1)
+blue = Color(0x0000ff,1)
+
+yellowOutline = LineStyle(1,yellow)
+
+yellowDimond = PolygonAsset([(0,0),(150,150),(300,0),(150,-150)],yellowOutline,yellow)
+text = TextAsset('Pedro Gallino', fill=blue, style='bold 30pt Times')
+
+Sprite(yellowDimond,(400,350))
+Sprite(text,(490,290))
+
+App().run()
 
