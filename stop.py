bb3595ee58254f54f39f9b4fb8e4c857a15ab165
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-26 14:54:17 -0400

    Updated from Brython Server: 9/26/2017 2:54:17 PM

--- stop.py
+++ stop.py
@@ -1,1 +1,14 @@
+#Pedro Gallino
+#9/20/17
+#stop.py - shows a stop sign
 
+
+from ggame import *
+
+red = Color(0xff0000,1)
+white = Color(0xffffff,1)
+
+redOutline = LineStyle(10,red)
+
+redOctagon = PolygonAsset([(0,0),(50,50),(60,0),(110,0),(],redOutline,red)
+text = TextAsset('Stop', fill=white, style='bold 40pt Times')
